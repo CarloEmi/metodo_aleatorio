9645102e47b2199c5b3f24d09a0da4aeaff43ef6
CarloEmi <carlosspereyra.cp@gmail.com>
2024-04-24 20:04:28 -0300

    vonneuman

--- app/Http/Controllers/vonNeumannController.php
+++ app/Http/Controllers/vonNeumannController.php
@@ -2,43 +2,61 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class VonNeumannController extends Controller
+class vonNeumannController extends Controller
 {
-    public function pruebaVonNeumann(Request $request)
-    {
-        $secuencia = $request->input('secuencia');
-
-        // Verificar si $secuencia es null o un array
-        if (!is_null($secuencia) && is_array($secuencia)) {
-            $paresIguales = 0;
-            $primerMenorSegundo = 0;
-            $segundoMenorPrimer = 0;
-            
-            $numElementos = count($secuencia);
-            for ($i = 0; $i < $numElementos - 1; $i++) {
-                if ($secuencia[$i] === $secuencia[$i + 1]) {
-                    $paresIguales++;
-                } elseif ($secuencia[$i] < $secuencia[$i + 1]) {
-                    $primerMenorSegundo++;
-                } else {
-                    $segundoMenorPrimer++;
-                }
-            }
-            
-            return view('pruebaNeumann', [
-                'pares_iguales' => $paresIguales,
-                'primer_menor_segundo' => $primerMenorSegundo,
-                'segundo_menor_primer' => $segundoMenorPrimer
-            ]);
-        } else {
-            // Manejar el caso en que $secuencia es null o no es un array
-            return redirect()->back()->with('error', 'La secuencia no es válida.');
-        }
+    public function index(){
+        return view('neumann.index');
     }
 
+    public function VonNeumann(Request $request){
+        
+        $sm1 = $request->semilla;
+        $cantidad = $request->cantidad;
+        $contador = 1;
 
-   
+        # Agregamos la primer semilla al resultado final
+        $resultado = str_split($sm1);
+
+        while($contador < $cantidad){
+            
+            // Elevamos al cuadrado la semilla
+            $cuadrado = pow($sm1, 2);
+
+            // Convertimos a texto el resultado y transformamos a un array
+            $resultadoTexto = strval($cuadrado);
+            $array = str_split($resultadoTexto);
+            
+
+            //Validamos que el resultado tengo 8 digitos
+            if(count($array) <  8){
+                while(count($array) < 8){
+                    array_unshift($array, "0");
+                }
+            }
+
+            // Extraemos los cuatro valores centrales
+            $sm1 = "";
+            for($i = 2; $i<6; $i++){
+                if($array[$i] == 0 && $array[$i+1] == 0){
+                    $sm1 = $sm1."1"."3";
+                    array_push($resultado, "1", "3");
+                    break;
+                }
+                $valor = $array[$i];
+                $sm1 = $sm1.$valor;
+                array_push($resultado, $valor);
+            }
+
+            $contador = $contador + 1;
+        }
+
+        // dd($resultado);
+        return view("neumann.tabla_resultados",[
+            "resultados" => $resultado, 
+            "semilla" => $request->semilla, 
+            "cantidad" => $cantidad]);
+    }
+
 }
